e5b8dd7ded1748f85574502795b9020c513cec28
Mahmoud Ali <mahmoud.althaqel@gmail.com>
2022-12-19 01:19:31 +0100

    CMakeLists.txt

---
+++ sim.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "G4RunManger.hh"
+
+
+int main(int argc, char** argv)
+{
+    G4RunManager *runManager = new G4RunManager();
+
+    G4UIExecutive *ui = new G4UIExecutive(argc, argv);
+
+    G4visManger
+
+    return 0;
+}
